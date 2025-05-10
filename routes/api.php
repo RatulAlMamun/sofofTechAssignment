9070bb86aae9b19602cdfd1135393adf1fed3a5d
RatulAlMamun <ratulalmamun23@gmail.com>
2025-05-10 12:27:18 +0600

    chore: add fallback routes.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BaseController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,8 @@ Route::controller(AuthController::class)->group(function (){
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', TaskController::class);
     Route::post('tasks/{id}/assign', [TaskController::class, 'assign']);
+});
+
+Route::fallback(function () {
+    return (new BaseController)->sendErrorJson('Not Found!');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\BaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return (new BaseController)->sendSuccessJson(
+        ['timestamp' => now(),],
+        'Task api running...'
+    );
+});
+
+Route::fallback(function () {
+    return (new BaseController)->sendErrorJson('Not Found!');
 });
